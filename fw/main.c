24de89da3a7b519a442fec42ad672a1448b7a617
dnbdmr <dnbdmr@gmail.com>
2020-09-14 18:49:42 -0700

    Tweak scrolllim

--- fw/main.c
+++ fw/main.c
@@ -296,20 +296,28 @@ void hid_task(void)
 			static uint8_t middle_last = 0;
 			static uint8_t middle_move = 0;
 			if ((mousekeys & MOUSE_BUTTON_MIDDLE) && !fn_key && (tpdata.x || tpdata.y)) { // Scroll if middle mouse pressed and movement
-				if (tpdata.x > config.scrolllim)
+				if (tpdata.x >= config.scrolllim)
 					tpdata.x = 1;
-				else if (tpdata.x < -1 * config.scrolllim)
+				else if (tpdata.x <= -config.scrolllim)
 					tpdata.x = -1;
-
-				if (tpdata.y > config.scrolllim)
-					tpdata.y = 1;
-				else if (tpdata.y < -1 * config.scrolllim)
-					tpdata.y = -1;
-
-				if (config.scrollswapxy)
-					tud_hid_mouse_report(REPORT_ID_MOUSE, (mousekeys & ~MOUSE_BUTTON_MIDDLE), 0, 0, tpdata.x/config.scrollscaley, tpdata.y/config.scrollscalex);
 				else
-					tud_hid_mouse_report(REPORT_ID_MOUSE, (mousekeys & ~MOUSE_BUTTON_MIDDLE), 0, 0, tpdata.y/config.scrollscaley, tpdata.x/config.scrollscalex);
+					tpdata.x = 0;
+
+				if (tpdata.y >= config.scrolllim)
+					tpdata.y = 1;
+				else if (tpdata.y <= -config.scrolllim)
+					tpdata.y = -1;
+				else
+					tpdata.y = 0;
+
+				if ( tpdata.x || tpdata.y ) {
+					if (config.scrollswapxy)
+						tud_hid_mouse_report(REPORT_ID_MOUSE, (mousekeys & ~MOUSE_BUTTON_MIDDLE), 0, 0, tpdata.x/config.scrollscaley, tpdata.y/config.scrollscalex);
+					else
+						tud_hid_mouse_report(REPORT_ID_MOUSE, (mousekeys & ~MOUSE_BUTTON_MIDDLE), 0, 0, tpdata.y/config.scrollscaley, tpdata.x/config.scrollscalex);
+				}
+
+				//Register as a move even if we only moved under scrolllim
 				middle_move = 1;
 				middle_last = 1;
 			}
